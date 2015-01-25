23f59a09d0f0c49c288f7cf3511995ab242ef3af
titofmax <titofmax@gmail.com>
2015-01-25 11:46:32 +0100

    managing users

--- client/client.js
+++ client/client.js
@@ -4,9 +4,10 @@ function styleSidenavSelectedItem() {
 	var url = Router.current().route._path;
 	var items = document.getElementsByClassName('side-nav-item');
     _.each(items, function(item) {
-      if( item.getAttribute('href') == url) {
-      	item.parentNode.setAttribute("class", "active");
-      }
+    	item.parentNode.removeAttribute("class", "active");
+		if( item.getAttribute('href') == url) {
+			item.parentNode.setAttribute("class", "active");
+		} 
     })
 }
 
